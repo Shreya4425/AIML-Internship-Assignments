52c78e0f5ecbf6b8b3fa25182f36eba7f2188b4e
Shreya4425 <157006473+Shreya4425@users.noreply.github.com>
2026-02-09 15:09:12 +0530

    Delete Day_5_Assignmanet directory

--- Day_5_Assignmanet/calc.py
+++
@@ -1,6 +0,0 @@
-class calculation:
-    def add(self, x, y):
-        return x + y
-
-    def product(self, x, y):
-        return x * y
--- Day_5_Assignmanet/measurements.py
+++
@@ -1,7 +0,0 @@
-class Measurements:
-
-    def area(self, length, width):
-        return length * width
-
-    def perimeter(self, length, width):
-        return 2 * (length + width)
